bcee695406d4ed13b7cf8fd51bce7d58331f7a4e
4ntm4n <anton.askling@gmail.com>
2024-05-04 04:11:30 +0200

    refactor letter_counter to count all chars instead

--- main.py
+++ main.py
@@ -2,29 +2,29 @@ def main():
     book_path = "books/frankenstein.txt"
     book_text = read_text(book_path)
     num_of_words = word_count(book_text)
-    num_of_letters = letter_count(book_text)
+    num_of_char = char_count(book_text)
     print(f"{num_of_words} words found in the document")
-    print(f"{num_of_letters}")
+    print(f"{num_of_char}")
 
-# made it count letters from the alphabet and not any char.
-def letter_count(text):
-    letters = "abcdefghijklmnopqrstuvwxyz"
-    letter_count = {}
-    for l in text:
-        lower_l = l.lower()
-        if lower_l in letters:
-            if lower_l in letter_count:
-                letter_count[lower_l] += 1
-            else:
-                letter_count[lower_l] = 1           
-    return letter_count
+def char_count(text):
+    char = {}
+    for c in text:
+        lc = c.lower()
+        if lc in char:
+            char[lc] += 1
+        else:
+            char[lc] = 1
+    return char
+
 
 def word_count(text):
     words_list = text.split()
     return len(words_list)
 
+
 def read_text(path):
     with open(path) as f:
         return f.read()
+
 
 main()
