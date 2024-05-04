9be709ba09ef6e284991e51ba950a064827745ce
4ntm4n <anton.askling@gmail.com>
2024-05-04 16:44:53 +0200

    add char_analyis and filter to char_count

--- main.py
+++ main.py
@@ -2,20 +2,40 @@ def main():
     book_path = "books/frankenstein.txt"
     book_text = read_text(book_path)
     num_of_words = word_count(book_text)
-    num_of_char = char_count(book_text)
-    print(f"{num_of_words} words found in the document")
-    print(f"{num_of_char}")
+    chars = char_count(book_text)
+    char_analysis = get_char_analysis(chars)
+
+  
+    print(f"This book has {num_of_words} words... \n")
+    print("Here comes a character analyis on this book:")
+    for ch in char_analysis:
+        print(f"character {ch["char"]} is found {ch["number"]} times")
+
+
+def get_num(dict):
+    return dict["number"]
+
+def get_char_analysis(dict):
+    stats = []
+    for key in dict:
+        stats.append({"char": key, "number": dict[key]})
+
+    stats.sort(key=get_num, reverse=True)
+    return stats
+    
 
 def char_count(text):
+    filter = "abcdefghiljlmnopqrstuvwxyz.:/"
     char = {}
     for c in text:
         lc = c.lower()
-        if lc in char:
-            char[lc] += 1
-        else:
-            char[lc] = 1
-    return char
+        if lc in filter:
+            if lc in char:
+                char[lc] += 1
+            else:
+                char[lc] = 1
 
+    return char
 
 def word_count(text):
     words_list = text.split()
